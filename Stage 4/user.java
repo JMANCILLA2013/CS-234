87b92db9256e07fdfc8fc629f28b506322423181
Palejrey <122762968+Palejrey@users.noreply.github.com>
2023-12-14 09:54:41 -0600

    Delete Stage 4/user.java

--- Stage 4/user.java
+++
@@ -1,8 +0,0 @@
-public interface user {
-    boolean login(String userInputEmail, String userInputPassword);
-    void logout();
-    void displayMenu(Database database);
-    String getEmail();
-    boolean isLoggedIn();
-    String getUserType();
-}
